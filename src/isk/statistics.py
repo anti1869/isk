fdc08de5b0740d8a948ccb26a1dd282e05224c34
Dmitry Litvinenko <anti1869@gmail.com>
2016-02-24 23:55:11 +0300

    Refreshed statistics module

--- src/isk/statistics.py
+++ src/isk/statistics.py
@@ -21,66 +21,66 @@
 #
 ###############################################################################
 
-import string
 import time
 import os
 
-def uptime ():
-    return string.atof (
-            string.split (
-                    open ('/proc/uptime', 'r').readline()
-                    )[1]
-            )
+
+def uptime():
+    result = float(str(open('/proc/uptime', 'r').readline()).split()[1])
+    return result
 
 
 # To iterate is human; to recurse, divine.
-def dhms(m,t):
-   if not t: return (m,)
-   return dhms(m//t[0], t[1:]) + (m % t[0],)
+def dhms(m, t):
+    if not t:
+        return m,
+    return dhms(m // t[0], t[1:]) + (m % t[0], )
+
 
 def human_readable(msec):
-    msec = msec * 1000
-    return '%d days %d hours %d minutes %d seconds'% dhms(msec//1000, (60, 60, 24))
+    msec *= 1000
+    return '%d days %d hours %d minutes %d seconds' % dhms(msec // 1000, (60, 60, 24))
+
 
 _proc_status = '/proc/%d/status' % os.getpid()
 
-_scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
-          'KB': 1024.0, 'MB': 1024.0*1024.0}
+_scale = {
+    'kB': 1024.0,
+    'mB': 1024.0 * 1024.0,
+    'KB': 1024.0,
+    'MB': 1024.0 * 1024.0
+}
+
 
 def _VmB(VmKey):
-    '''Private.
-    '''
+    """Private."""
     global _proc_status, _scale
-     # get pseudo file  /proc/<pid>/status
+    # get pseudo file  /proc/<pid>/status
     try:
         t = open(_proc_status)
         v = t.read()
         t.close()
     except:
         return 0.0  # non-Linux?
-     # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
+        # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
     i = v.index(VmKey)
     v = v[i:].split(None, 3)  # whitespace
     if len(v) < 3:
         return 0.0  # invalid format?
-     # convert Vm value to bytes
+        # convert Vm value to bytes
     return float(v[1]) * _scale[v[2]]
 
 
 def memory(since=0.0):
-    '''Return memory usage in bytes.
-    '''
+    """Return memory usage in bytes."""
     return _VmB('VmSize:') - since
 
 
 def resident(since=0.0):
-    '''Return resident memory usage in bytes.
-    '''
+    """Return resident memory usage in bytes."""
     return _VmB('VmRSS:') - since
 
 
 def stacksize(since=0.0):
-    '''Return stack size in bytes.
-    '''
+    """Return stack size in bytes."""
     return _VmB('VmStk:') - since
-
