602bd9af6bca380f3068c2ab324990450e4be835
Dmitry Litvinenko <anti1869@gmail.com>
2016-02-29 11:02:02 +0300

    Refactored BaseDBView

--- src/isk/web/rest/views/db.py
+++ src/isk/web/rest/views/db.py
@@ -2,100 +2,10 @@
 DB Management REST API views.
 """
 
-from abc import ABCMeta, abstractmethod
-import asyncio
-from datetime import datetime
-from functools import partial
-from typing import Callable, Any
-
 from aiohttp import web_exceptions
 
-from sunhead.rest.views import JSONView
-
 from isk.api import db as db_api
-from isk.api.executor import isk_api_executor
-
-
-def time_taken(f):
-    """
-    Adds "time_taken" key to the dict, returned by wrapped function.
-    """
-    async def _wrapper(*args, **kwargs) -> dict:
-        d1 = datetime.now()
-        result = await f(*args, **kwargs)
-        d2 = datetime.now()
-        delta = d2 - d1
-        result["time_taken"] = delta.total_seconds()
-        return result
-    return _wrapper
-
-
-
-class BaseDBView(JSONView, metaclass=ABCMeta):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._loop = asyncio.get_event_loop()
-
-    def _get_default_context_futures(self) -> list:
-        fs = [
-            self._get_default_context_data(),
-        ]
-        return fs
-
-    async def _get_default_context_data(self):
-        data = {
-            "requested": {
-                "db_id": self.requested_db_id,
-            }
-        }
-        return data
-
-    async def get_db_list(self) -> dict:
-        db_list = await self._loop.run_in_executor(isk_api_executor, db_api.get_db_detailed_list)
-        data = {
-            "databases_list": db_list,
-        }
-        return data
-
-    @abstractmethod
-    def _get_context_futures(self) -> list:
-        return []
-
-    @property
-    def requested_db_id(self):
-        """Shortcut for extracting db id from request"""
-        return int(self.request.match_info.get("db_id", 0))
-
-    async def _hit_api(self, func: Callable, *args) -> Any:
-        """
-        Wrap API call into executor. Saves some typing.
-
-        :param func: API Function to run
-        :param args: Arguments to that function
-        :return: Result of the function
-        """
-        fetcher = partial(func, *args)
-        result = await self._loop.run_in_executor(isk_api_executor, fetcher)
-        return result
-
-    @time_taken
-    async def _get_context_data(self) -> dict:
-        fs = self._get_default_context_futures() + self._get_context_futures()
-        data = {}
-        if not len(fs):
-            return data
-
-        for f in asyncio.as_completed(fs):
-            result = await f
-            if result is not None:
-                data.update(result)
-
-        return data
-
-    async def get(self):
-        ctx = await self._get_context_data()
-        return self.json_response(context_data=ctx)
+from isk.web.rest.views.generic import BaseDBView
 
 
 class DBListView(BaseDBView):
@@ -107,17 +17,16 @@ class DBListView(BaseDBView):
         return fs
 
     async def make_new_db_id(self):
-        id_list = await self._loop.run_in_executor(isk_api_executor, db_api.get_db_list)
+        id_list = await self._hit_api(db_api.get_db_list)
         new_id = max(id_list) + 1
         return new_id
 
     async def post(self):
         new_db_id = await self.make_new_db_id()
-        db_creator = partial(db_api.create_db, new_db_id)
-        created_id = await self._loop.run_in_executor(isk_api_executor, db_creator)
+        created_id = await self._hit_api(db_api.create_db, new_db_id)
         assert created_id == new_db_id
-        await self._loop.run_in_executor(isk_api_executor, db_api.save_all_dbs)
-        new_url = "./{}/".format(new_db_id)
+        await self._hit_api(db_api.save_all_dbs)
+        new_url = "./{}/".format(new_db_id)  # FIXME: Use reverse url resolving here
         return self.created_response(location=new_url)
 
 
@@ -128,10 +37,8 @@ class DBView(BaseDBView):
         if self.requested_db_id not in db_list["databases_list"]:
             raise web_exceptions.HTTPNotFound
 
-        counter = partial(db_api.get_db_img_count, self.requested_db_id)
-        image_count = await self._loop.run_in_executor(isk_api_executor, counter)
+        image_count = await self._hit_api(db_api.get_db_img_count, self.requested_db_id)
         data = {
-            "requested_db_id": self.requested_db_id,
             "db_info": db_list["databases_list"].get(self.requested_db_id, {}),
             "image_count": image_count,
         }
@@ -144,7 +51,6 @@ class DBView(BaseDBView):
         return fs
 
     async def delete(self):
-        terminator = partial(db_api.remove_db, self.requested_db_id)
-        result = await self._loop.run_in_executor(isk_api_executor, terminator)
-        assert result
+        terminator = await self._hit_api(db_api.remove_db, self.requested_db_id)
+        assert terminator
         raise web_exceptions.HTTPNoContent
--- src/isk/web/rest/views/generic.py
+++ src/isk/web/rest/views/generic.py
@@ -1,11 +1,100 @@
 """
 Helpers & generics
 """
+from abc import ABCMeta, abstractmethod
+import asyncio
+from datetime import datetime
+from functools import partial
+from typing import Callable, Any
 
 from sunhead.rest.views import JSONView
+
+from isk.api import db as db_api
+from isk.api.executor import isk_api_executor
+
+
+def time_taken(f):
+    """
+    Adds "time_taken" key to the dict, returned by wrapped function.
+    """
+    async def _wrapper(*args, **kwargs) -> dict:
+        d1 = datetime.now()
+        result = await f(*args, **kwargs)
+        d2 = datetime.now()
+        delta = d2 - d1
+        result["time_taken"] = delta.total_seconds()
+        return result
+    return _wrapper
 
 
 class NotImplementedView(JSONView):
 
     async def get(self):
         return self.json_response({})
+
+
+class BaseDBView(JSONView, metaclass=ABCMeta):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._loop = asyncio.get_event_loop()
+
+    def _get_default_context_futures(self) -> list:
+        fs = [
+            self._get_default_context_data(),
+        ]
+        return fs
+
+    async def _get_default_context_data(self):
+        data = {
+            "requested": {
+                "db_id": self.requested_db_id,
+            }
+        }
+        return data
+
+    async def get_db_list(self) -> dict:
+        db_list = await self._hit_api(db_api.get_db_detailed_list)
+        data = {
+            "databases_list": db_list,
+        }
+        return data
+
+    @abstractmethod
+    def _get_context_futures(self) -> list:
+        return []
+
+    @property
+    def requested_db_id(self):
+        """Shortcut for extracting db id from request"""
+        return int(self.request.match_info.get("db_id", 0))
+
+    async def _hit_api(self, func: Callable, *args) -> Any:
+        """
+        Wrap API call into executor. Saves some typing.
+
+        :param func: API Function to run
+        :param args: Arguments to that function
+        :return: Result of the function
+        """
+        fetcher = partial(func, *args)
+        result = await self._loop.run_in_executor(isk_api_executor, fetcher)
+        return result
+
+    @time_taken
+    async def _get_context_data(self) -> dict:
+        fs = self._get_default_context_futures() + self._get_context_futures()
+        data = {}
+        if not len(fs):
+            return data
+
+        for f in asyncio.as_completed(fs):
+            result = await f
+            if result is not None:
+                data.update(result)
+
+        return data
+
+    async def get(self):
+        ctx = await self._get_context_data()
+        return self.json_response(context_data=ctx)
