5424ae82b828a4c41bae7b2a23aeed2e1d173fb7
Dmitry Litvinenko <anti1869@gmail.com>
2016-03-01 11:47:24 +0300

    Adding from filem but need to rework

--- src/isk/web/rest/views/images.py
+++ src/isk/web/rest/views/images.py
@@ -58,23 +58,26 @@ class ImagesListView(BaseImagesView):
         fs = [getattr(self, "add_from_{}".format(key)) for key in processing_keys if key in data]
 
         # TODO: Try another approach here with wait or something like that
+        # TODO: Prevent clashes of different add methods (image_id, for example)
         for f in fs:
             asyncio.ensure_future(f(data))
 
         raise web_exceptions.HTTPAccepted
 
-    async def add_from_download_url(self, data) -> None:
+    def _get_image_id_from_post_data(self, data) -> int:
         try:
             image_id = int(data.get("image_id", 0))
             assert image_id
         except (AssertionError, ValueError):
-            logger.error("No image id provided in field 'image_id' when requested to download url", exc_info=True)
+            logger.error("No image id provided in field 'image_id'", exc_info=True)
             return
 
+    async def add_from_download_url(self, data) -> None:
         url = data["download_url"]
+        image_id = self._get_image_id_from_post_data(data)
         logger.debug("Adding from download_url=%s", url)
         filename = os.path.basename(urlsplit(url).path)  # Or we can use tmpfile here
-        local_path = os.path.join(settings.TMP_DIR, filename)
+        local_path = os.path.join(settings.TMP_DIR, filename)  # FIXME: remove tmp file after add
         downloaded_path = await download_image(url, local_path)
         if not downloaded_path:
             logger.error("Image is not added from url '%s'", url)
@@ -84,10 +87,18 @@ class ImagesListView(BaseImagesView):
         assert result
         logger.info("Added id=%s from url %s", image_id, url)
 
-
     async def add_from_image(self, data):
         image_field = data["image"]
-        logger.info("Adding from image filename=%s", image_field.filename)
+        image_id = self._get_image_id_from_post_data(data)
+        logger.debug("Adding from image filename=%s", image_field.filename)
+        local_path = os.path.join(settings.TMP_DIR, image_field.filename)
+        with open(local_path, 'wb') as f:
+            for l in image_field.file:
+                f.write(l)
+
+        result = await self._hit_api(images_api.add_img, self.requested_db_id, image_id, local_path)
+        assert result
+        logger.debug("Added id=%s", image_id)
 
     async def add_from_archive(self, data):
         archive_field = data["archive"]
