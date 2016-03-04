7e1c2d924cc7fe4d9e6a57f1507fcdaa52c0d0d9
Dmitry Litvinenko <anti1869@gmail.com>
2016-03-04 12:05:38 +0300

    Added executor note. ProcessPool is faster, but is it safe?

--- src/isk/api/executor.py
+++ src/isk/api/executor.py
@@ -6,7 +6,7 @@ As long Isk API is blocking, we need to run its calls with executor.
 from concurrent.futures import ThreadPoolExecutor
 
 
-MAX_SIMULTANEOUS_ISK_API_CALLS = 5
+MAX_SIMULTANEOUS_ISK_API_CALLS = 1  # No diff with ThreadPoolExecutor, migrate to Process pool
 
 
 isk_api_executor = ThreadPoolExecutor(max_workers=MAX_SIMULTANEOUS_ISK_API_CALLS)
