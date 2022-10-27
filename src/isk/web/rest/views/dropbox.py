9f8ef805d5f857bed02f41a7f1051570a874116c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 13:15:10 +0000

    Adding tarfile member sanitization to extractall()

--- src/isk/web/rest/views/dropbox.py
+++ src/isk/web/rest/views/dropbox.py
@@ -118,7 +118,26 @@ class ImagesDropboxTgz(ImagesDropboxArchive):
     async def _unarchive(self, archive_path: str, cleanup: bool = True) -> None:
         dest = os.path.dirname(archive_path)
         with tarfile.open(archive_path) as tar:
-            tar.extractall(path=dest, members=self._unarchive_members(tar))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=dest, members=self._unarchive_members(tar))
         if cleanup:
             os.remove(archive_path)
 
